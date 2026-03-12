15d64917d9bd20f563fc2f9960e9c680af01771a
vannkorn <vannkorn@gmail.com>
2026-03-12 22:29:46 +0700

    Hide out unused post type

--- inc/ppfertilizer/__post-types.php
+++ inc/ppfertilizer/__post-types.php
@@ -5,6 +5,6 @@
 // require get_template_directory() . '/inc/ppfertilizer/post-types/story.php';
 // require get_template_directory() . '/inc/ppfertilizer/post-types/service-provider.php';
 require get_template_directory() . '/inc/ppfertilizer/post-types/product.php';
-require get_template_directory() . '/inc/ppfertilizer/post-types/team.php';
+// require get_template_directory() . '/inc/ppfertilizer/post-types/team.php';
 require get_template_directory() . '/inc/ppfertilizer/post-types/career.php';
 // require get_template_directory() . '/inc/ppfertilizer/post-types/video.php';
